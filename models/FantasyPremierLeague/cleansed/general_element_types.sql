862c8c0e7bb4735ba2950fdfd26179a9e93aaa17
André Morais <andre.95morais@gmail.com>
2025-09-24 10:31:38 +0000

    update sql style of general element types

--- models/FantasyPremierLeague/cleansed/general_element_types.sql
+++ models/FantasyPremierLeague/cleansed/general_element_types.sql
@@ -1,17 +1,19 @@
-With
-  deduplication as (
-    SELECT
-      id
-      , singular_name_short
-      , singular_name
-      , element_count
-      , row_number() over (partition by id order by extraction_timestamp desc) as sort_latest_record
-    FROM `raw_fpl.raw_fpl_element_types`
-  )
-  SELECT
+with
+    deduplication as (
+        select
+            id,
+            singular_name_short,
+            singular_name,
+            element_count,
+            row_number() over (
+                partition by id order by extraction_timestamp desc
+            ) as sort_latest_record
+        from `raw_fpl.raw_fpl_element_types`
+    )
+select 
     id
     , singular_name_short
     , singular_name
     , element_count
-  FROM deduplication
-  WHERE sort_latest_record = 1
+from deduplication
+where sort_latest_record = 1

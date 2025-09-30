338972d7a11a0f207954fbbc54df48aa6b43cff9
André Morais <andre.95morais@gmail.com>
2025-09-30 11:32:15 +0000

    initiate additional cleansed tables

---
+++ models/FantasyPremierLeague/cleansed/cleansed_element_summaries_round.sql
@@ -0,0 +1,90 @@
+with deduplication as (
+  SELECT
+    element
+    , round
+    , fixture
+    , was_home
+    , opponent_team
+    , team_h_score
+    , team_a_score
+    , kickoff_time
+    , total_points
+    , bps
+    , bonus
+    , starts
+    , minutes
+    , goals_scored
+    , expected_goals
+    , assists
+    , expected_assists
+    , expected_goal_involvements
+    , goals_conceded
+    , expected_goals_conceded
+    , clean_sheets
+    , saves
+    , own_goals
+    , penalties_saved
+    , penalties_missed
+    , tackles
+    , recoveries
+    , clearances_blocks_interceptions
+    , defensive_contribution
+    , yellow_cards
+    , red_cards
+    , ict_index
+    , threat
+    , creativity
+    , influence
+    , transfers_in
+    , transfers_out
+    , transfers_balance
+    , selected
+    , value
+    , modified
+    , row_number() over(partition by element order by extraction_timestamp desc) as sort_latest_record
+  FROM `fantasy-premier-league-469511.raw_fpl.raw_fpl_element_summaries_round`
+)
+SELECT
+    element
+    , round
+    , fixture
+    , was_home
+    , opponent_team
+    , team_h_score
+    , team_a_score
+    , kickoff_time
+    , total_points
+    , bps
+    , bonus
+    , starts
+    , minutes
+    , goals_scored
+    , expected_goals
+    , assists
+    , expected_assists
+    , expected_goal_involvements
+    , goals_conceded
+    , expected_goals_conceded
+    , clean_sheets
+    , saves
+    , own_goals
+    , penalties_saved
+    , penalties_missed
+    , tackles
+    , recoveries
+    , clearances_blocks_interceptions
+    , defensive_contribution
+    , yellow_cards
+    , red_cards
+    , ict_index
+    , threat
+    , creativity
+    , influence
+    , transfers_in
+    , transfers_out
+    , transfers_balance
+    , selected
+    , value
+    , modified
+FROM deduplication
+WHERE sort_latest_record = 1

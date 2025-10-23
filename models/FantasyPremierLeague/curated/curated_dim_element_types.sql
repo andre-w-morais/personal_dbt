5fee2e4edc07e63ca7f2db3b90a2f0762e92bc7e
André Morais <andre.95morais@gmail.com>
2025-10-23 17:11:03 +0000

    Updated downstream dependency of renaming model from "cleansed_general_element_types" to "cleansed_element_types"

--- models/FantasyPremierLeague/curated/curated_dim_element_types.sql
+++ models/FantasyPremierLeague/curated/curated_dim_element_types.sql
@@ -3,4 +3,4 @@ SELECT
     , singular_name_short as short_type_name
     , singular_name as type_name
     , element_count
-FROM {{ref("cleansed_general_element_types")}}
+FROM {{ref("cleansed_element_types")}}

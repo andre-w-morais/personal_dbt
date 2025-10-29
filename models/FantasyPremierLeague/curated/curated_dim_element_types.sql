faba6bf1baf3a61b9f94926bcefb83ac8274adec
André Morais <andre.95morais@gmail.com>
2025-10-29 19:22:22 +0000

    fix bug

--- models/FantasyPremierLeague/curated/curated_dim_element_types.sql
+++ models/FantasyPremierLeague/curated/curated_dim_element_types.sql
@@ -1,6 +1,6 @@
 SELECT
     element_type_id
-    , singular_name_short as short_element_type_name
-    , singular_name as element_type_name
+    , short_element_type_name
+    , element_type_name
     , element_type_count
 FROM {{ref("cleansed_element_types")}}

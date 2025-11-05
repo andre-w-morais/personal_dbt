4941d05e9a3dfcc9494cc88067c7a09537bbc022
André Morais <andre.95morais@gmail.com>
2025-11-05 15:19:35 +0000

    Refine data mart player_performance_rounds.sql

--- models/FantasyPremierLeague/marts/player_performance_rounds.sql
+++ models/FantasyPremierLeague/marts/player_performance_rounds.sql
@@ -2,20 +2,19 @@ SELECT
     fes.element_id
     , de.element_web_name
     , CONCAT(de.element_first_name, ' ', de.element_second_name) AS element_full_name
-    , fes.event_id
+    , det.short_element_type_name AS element_type
+    , dt.short_team_name AS team
+    , dot.short_team_name AS opponent_team
+    , fes.event_id AS gameweek
+        , CASE
+        WHEN was_home THEN fes.team_h_score
+        ELSE fes.team_a_score
+        END AS team_score
+    , CASE
+        WHEN was_home THEN fes.team_a_score
+        ELSE fes.team_h_score
+        END AS opponent_team_score
     , fes.kickoff_at
-    , dev.event_name
-    , dt.short_team_name AS opponent_team
-    , dt.strength AS opponent_team_strength
-    , dt.strength_overall_home AS opponent_team_strength_overall_home
-    , dt.strength_overall_away AS opponent_team_strength_overall_away
-    , dt.strength_attack_home AS opponent_team_strength_attack_home
-    , dt.strength_attack_away AS opponent_team_strength_attack_away
-    , dt.strength_defence_home AS opponent_team_strength_defence_home
-    , dt.strength_defence_away AS opponent_team_strength_defence_away
-    , fes.was_home
-    , fes.team_h_score
-    , fes.team_a_score
     , fes.total_points
     , fes.bps
     , fes.bonus
@@ -43,6 +42,13 @@ SELECT
     , fes.threat
     , fes.creativity
     , fes.influence
+    , dot.strength AS opponent_team_strength
+    , dot.strength_overall_home AS opponent_team_strength_overall_home
+    , dot.strength_overall_away AS opponent_team_strength_overall_away
+    , dot.strength_attack_home AS opponent_team_strength_attack_home
+    , dot.strength_attack_away AS opponent_team_strength_attack_away
+    , dot.strength_defence_home AS opponent_team_strength_defence_home
+    , dot.strength_defence_away AS opponent_team_strength_defence_away
     , fes.transfers_in
     , fes.transfers_out
     , fes.transfers_balance
@@ -50,5 +56,5 @@ SELECT
 FROM {{ ref("curated_fact_element_summaries_rounds") }} AS fes
     LEFT JOIN {{ref("curated_dim_elements")}} AS de ON de.element_id = fes.element_id AND (fes.kickoff_at BETWEEN de.valid_from AND de.valid_to)
     LEFT JOIN {{ref("curated_dim_element_types")}} AS det ON det.element_type_id = fes.element_type_id AND (fes.kickoff_at BETWEEN det.valid_from AND det.valid_to)
-    LEFT JOIN {{ref("curated_dim_events")}} AS dev ON dev.event_id = fes.event_id AND (fes.kickoff_at BETWEEN dev.valid_from AND dev.valid_to)
-    LEFT JOIN {{ref("curated_dim_teams")}} AS dt ON dt.team_id = fes.opponent_team_id AND (fes.kickoff_at BETWEEN dt.valid_from AND dt.valid_to)
+    LEFT JOIN {{ref("curated_dim_teams")}} AS dot ON dot.team_id = fes.opponent_team_id AND (fes.kickoff_at BETWEEN dot.valid_from AND dot.valid_to)
+    LEFT JOIN {{ref("curated_dim_teams")}} AS dt ON dt.team_id = fes.team_id AND (fes.kickoff_at BETWEEN dt.valid_from AND dt.valid_to)
